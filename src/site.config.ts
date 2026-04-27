3ec578f44cae7b23dc02959120b67a5cf8fbf09f
salihbektas <sbektas@gtu.edu.tr>
2026-04-27 23:11:47 +0300

    chore: update site configuration and metadata to reflect personal domain migration

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,24 +1,24 @@
-import type { SiteConfig } from '~/types'
+import type { SiteConfig } from "~/types"
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: "https://salihbektas.github.io",
   // The name of your site, used in the title and for SEO.
-  title: 'MultiTerm',
+  title: "MultiTerm",
   // The description of your site, used for SEO and RSS feed.
   description:
-    'A coder-ready Astro blog theme with 59 of your favorite color schemes to choose from',
+    "A coder-ready Astro blog theme with 59 of your favorite color schemes to choose from",
   // The author of the site, used in the footer, SEO, and RSS feed.
-  author: 'Katy Kookaburra',
+  author: "Salih Bektas",
   // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'Terminal', 'Theme', 'MultiTerm', 'stelcodes'],
+  tags: ["Astro", "Terminal", "Theme", "MultiTerm", "stelcodes"],
   // Path to the image used for generating social media previews.
   // Needs to be a square JPEG file due to limitations of the social card generator.
   // Try https://squoosh.app/ to easily convert images to JPEG.
-  socialCardAvatarImage: './src/content/avatar.jpg',
+  socialCardAvatarImage: "./src/content/avatar.jpg",
   // Font imported from @fontsource or elsewhere, used for the entire site.
   // To change this see src/styles/global.css and import a different font.
-  font: 'JetBrains Mono Variable',
+  font: "JetBrains Mono Variable",
   // For pagination, the number of posts to display per page.
   // The homepage will display half this number in the "Latest Posts" section.
   pageSize: 6,
@@ -29,30 +29,30 @@ const config: SiteConfig = {
   // The navigation links to display in the header.
   navLinks: [
     {
-      name: 'Home',
-      url: '/',
+      name: "Home",
+      url: "/",
     },
     {
-      name: 'About',
-      url: '/about',
+      name: "About",
+      url: "/about",
     },
     {
-      name: 'Archive',
-      url: '/posts',
+      name: "Archive",
+      url: "/posts",
     },
     {
-      name: 'GitHub',
-      url: 'https://github.com/stelcodes/multiterm-astro',
+      name: "GitHub",
+      url: "https://github.com/salihbektas",
       external: true,
     },
   ],
   // The theming configuration for the site.
   themes: {
     // The theming mode. One of "single" | "select" | "light-dark-auto".
-    mode: 'select',
+    mode: "select",
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
-    default: 'catppuccin-mocha',
+    default: "catppuccin-mocha",
     // Shiki themes to bundle with the site.
     // https://expressive-code.com/guides/themes/#using-bundled-themes
     // These will be used to theme the entire site along with syntax highlighting.
@@ -62,66 +62,66 @@ const config: SiteConfig = {
     //   'github-dark',
     // ]
     include: [
-      'andromeeda',
-      'aurora-x',
-      'ayu-dark',
-      'catppuccin-frappe',
-      'catppuccin-latte',
-      'catppuccin-macchiato',
-      'catppuccin-mocha',
-      'dark-plus',
-      'dracula',
-      'dracula-soft',
-      'everforest-dark',
-      'everforest-light',
-      'github-dark',
-      'github-dark-default',
-      'github-dark-dimmed',
-      'github-dark-high-contrast',
-      'github-light',
-      'github-light-default',
-      'github-light-high-contrast',
-      'gruvbox-dark-hard',
-      'gruvbox-dark-medium',
-      'gruvbox-dark-soft',
-      'gruvbox-light-hard',
-      'gruvbox-light-medium',
-      'gruvbox-light-soft',
-      'houston',
-      'kanagawa-dragon',
-      'kanagawa-lotus',
-      'kanagawa-wave',
-      'laserwave',
-      'light-plus',
-      'material-theme',
-      'material-theme-darker',
-      'material-theme-lighter',
-      'material-theme-ocean',
-      'material-theme-palenight',
-      'min-dark',
-      'min-light',
-      'monokai',
-      'night-owl',
-      'nord',
-      'one-dark-pro',
-      'one-light',
-      'plastic',
-      'poimandres',
-      'red',
-      'rose-pine',
-      'rose-pine-dawn',
-      'rose-pine-moon',
-      'slack-dark',
-      'slack-ochin',
-      'snazzy-light',
-      'solarized-dark',
-      'solarized-light',
-      'synthwave-84',
-      'tokyo-night',
-      'vesper',
-      'vitesse-black',
-      'vitesse-dark',
-      'vitesse-light',
+      "andromeeda",
+      "aurora-x",
+      "ayu-dark",
+      "catppuccin-frappe",
+      "catppuccin-latte",
+      "catppuccin-macchiato",
+      "catppuccin-mocha",
+      "dark-plus",
+      "dracula",
+      "dracula-soft",
+      "everforest-dark",
+      "everforest-light",
+      "github-dark",
+      "github-dark-default",
+      "github-dark-dimmed",
+      "github-dark-high-contrast",
+      "github-light",
+      "github-light-default",
+      "github-light-high-contrast",
+      "gruvbox-dark-hard",
+      "gruvbox-dark-medium",
+      "gruvbox-dark-soft",
+      "gruvbox-light-hard",
+      "gruvbox-light-medium",
+      "gruvbox-light-soft",
+      "houston",
+      "kanagawa-dragon",
+      "kanagawa-lotus",
+      "kanagawa-wave",
+      "laserwave",
+      "light-plus",
+      "material-theme",
+      "material-theme-darker",
+      "material-theme-lighter",
+      "material-theme-ocean",
+      "material-theme-palenight",
+      "min-dark",
+      "min-light",
+      "monokai",
+      "night-owl",
+      "nord",
+      "one-dark-pro",
+      "one-light",
+      "plastic",
+      "poimandres",
+      "red",
+      "rose-pine",
+      "rose-pine-dawn",
+      "rose-pine-moon",
+      "slack-dark",
+      "slack-ochin",
+      "snazzy-light",
+      "solarized-dark",
+      "solarized-light",
+      "synthwave-84",
+      "tokyo-night",
+      "vesper",
+      "vitesse-black",
+      "vitesse-dark",
+      "vitesse-light",
     ],
     // Optional overrides for specific themes to customize colors.
     // Their values can be either a literal color (hex, rgb, hsl) or another theme key.
@@ -151,12 +151,12 @@ const config: SiteConfig = {
   },
   // Social links to display in the footer.
   socialLinks: {
-    github: 'https://github.com/stelcodes/multiterm-astro',
-    mastodon: 'https://github.com/stelcodes/multiterm-astro',
-    email: 'https://github.com/stelcodes/multiterm-astro',
-    linkedin: 'https://github.com/stelcodes/multiterm-astro',
-    bluesky: 'https://github.com/stelcodes/multiterm-astro',
-    twitter: 'https://github.com/stelcodes/multiterm-astro',
+    github: "https://github.com/salihbektas",
+    mastodon: "https://github.com/salihbektas",
+    email: "https://github.com/salihbektas",
+    linkedin: "https://github.com/salihbektas",
+    bluesky: "https://github.com/salihbektas",
+    twitter: "https://github.com/salihbektas",
     rss: true, // Set to true to include an RSS feed link in the footer
   },
   // Configuration for Giscus comments.
@@ -166,10 +166,10 @@ const config: SiteConfig = {
   // IMPORTANT: Update giscus.json in the root of the project with your own website URL
   // If you don't want to use Giscus, set this to undefined.
   giscus: {
-    repo: 'stelcodes/multiterm-astro',
-    repoId: 'R_kgDOPNnBig',
-    category: 'Giscus',
-    categoryId: 'DIC_kwDOPNnBis4CteOc',
+    repo: "stelcodes/multiterm-astro",
+    repoId: "R_kgDOPNnBig",
+    category: "Giscus",
+    categoryId: "DIC_kwDOPNnBis4CteOc",
     reactionsEnabled: true, // Enable reactions on post itself
   },
   // These are characters available for the character chat feature.
@@ -177,9 +177,9 @@ const config: SiteConfig = {
   // Make sure to compress the image to a web-friendly size (<100kb)
   // Try using the excellent https://squoosh.app web app for creating small webp files
   characters: {
-    owl: '/owl.webp',
-    unicorn: '/unicorn.webp',
-    duck: '/duck.webp',
+    owl: "/owl.webp",
+    unicorn: "/unicorn.webp",
+    duck: "/duck.webp",
   },
 }
 
