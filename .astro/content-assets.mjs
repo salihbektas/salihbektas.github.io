c59e68422da64f5fd7460f00b1a75bb37dd2c6d6
salihbektas <sbektas@gtu.edu.tr>
2026-05-13 01:29:37 +0300

    chore: localize tag labels to Turkish

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
+import __ASTRO_IMAGE_IMPORT_ZpRc0 from "./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md";
 import __ASTRO_IMAGE_IMPORT_Z1Glz99 from "./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md";
 import __ASTRO_IMAGE_IMPORT_NYJzW from "./canavarScreens.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md";
 import __ASTRO_IMAGE_IMPORT_CM76c from "./yaratik.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_ZpRc0 from "./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md";
-export default new Map([["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z1Glz99], ["./canavarScreens.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md", __ASTRO_IMAGE_IMPORT_NYJzW], ["./yaratik.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md", __ASTRO_IMAGE_IMPORT_CM76c], ["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_ZpRc0]]);
+export default new Map([["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_ZpRc0], ["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z1Glz99], ["./canavarScreens.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md", __ASTRO_IMAGE_IMPORT_NYJzW], ["./yaratik.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FDIY-car%2Findex.md", __ASTRO_IMAGE_IMPORT_CM76c]]);
 		
